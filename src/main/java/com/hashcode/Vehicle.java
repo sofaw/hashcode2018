a491ea7f615d9ef654f5ae8d4f6dbf726ad844aa
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-03-01 20:19:43 +0000

    separated scheduler

--- src/main/java/com/hashcode/Vehicle.java
+++ src/main/java/com/hashcode/Vehicle.java
@@ -1,10 +1,7 @@
 package com.hashcode;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Objects;
-import java.util.Queue;
 import java.util.Queue;
 
 public class Vehicle {
