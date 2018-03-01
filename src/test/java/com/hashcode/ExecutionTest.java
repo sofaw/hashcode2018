6615b564ada911459c070909e259ca1be797e0dd
sofaw <soph.wood52@gmail.com>
2018-03-01 19:21:26 +0000

    Add filepaths.

--- src/test/java/com/hashcode/ExecutionTest.java
+++ src/test/java/com/hashcode/ExecutionTest.java
@@ -12,23 +12,23 @@ public class ExecutionTest {
 
     @Test
     public void runb(){
-        Scheduler.schedule("");
+        Scheduler.schedule("src/main/resources/b_should_be_easy.in");
     }
 
 
     @Test
     public void runc(){
-        Scheduler.schedule("");
+        Scheduler.schedule("src/main/resources/c_no_hurry.in");
     }
 
     @Test
     public void rund() {
-        Scheduler.schedule("");
+        Scheduler.schedule("src/main/resources/d_metropolis.in");
     }
 
     @Test
     public void rune() {
-        Scheduler.schedule("");
+        Scheduler.schedule("src/main/resources/e_high_bonus.in");
     }
 }
 
